49c47cb46dfbfba4dc0dc06192cf41e3c22a1ca9
master-code-java <delllg1122@yahoo.com>
2021-06-12 22:34:51 +0100

    first commit

---
+++ src/main/java/com/oiseau/App.java
@@ -0,0 +1,13 @@
+package com.oiseau;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
